37ebe905b429549b9d378fd20bca55cbdca7a5f4
Robert Ris <take1risk@gmail.com>
2019-12-31 14:39:15 -0600

    use class component and pass method between components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,51 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  };
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
+  };
+
+
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a react app</h1>
-        <Person name="Bob" age="30" />
-        <Person name="Bill" age="32" />
-        <Person name="Joe" age="29">Hobbies: Hiking</Person>
-      </div>
+ 		<button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+     </div>
     );
   }
 }
