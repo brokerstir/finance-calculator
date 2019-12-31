00ed76fa769780352413855d12f09ca5a4fd93a5
Robert Ris <take1risk@gmail.com>
2019-12-31 13:56:37 -0600

    use functional component with useState hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const App = props => {
+
+	const [ personsState, setPersonsState ] = useState({
+	  	persons: [
+	  	    { name: 'Bob', age: 29 },
+	  	    { name: 'Joe', age: 33 },
+	  	    { name: 'Doug', age: 40 }
+	  	],
+	  	otherPerson: 'Fred'
+	});
+
+	const [otherState, setOtherState] = useState('foo bar');
+
+	console.log(personsState, otherState)
+
+	const switchNameHandler = () => {
+		setPersonsState({
+		  persons: [
+		  	  { name: 'Hank', age: 33 },
+		  	  { name: 'Tom', age: 25 },
+		  	  { name: 'Doug', age: 40 }
+			  ]
+		});
+
+		setOtherState(
+		  'Hello, world!'
+		);
+	};
+
     return (
       <div className="App">
         <h1>Hi, I am a react app</h1>
-        <Person name="Bob" age="30" />
-        <Person name="Bill" age="32" />
-        <Person name="Joe" age="29">Hobbies: Hiking</Person>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
       </div>
     );
-  }
 }
 
 export default App;
+
+
