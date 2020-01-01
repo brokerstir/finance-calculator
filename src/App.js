2a06596a1f0dbc9eb8fbc0d811ae4c63023cc99c
Robert Ris <take1risk@gmail.com>
2020-01-01 11:01:25 -0600

    add css styling

--- src/App.js
+++ src/App.js
@@ -40,10 +40,21 @@ class App extends Component {
 
 
   render() {
+
+  	const style = {
+  		backgroundColor: 'white',
+  		font: 'inherit',
+  		border: '1px solid blue',
+  		padding: '8px',
+  		cursor: 'pointer'
+  	}
+
     return (
       <div className="App">
         <h1>Hi, I am a react app</h1>
- 		<button onClick={ () => this.switchNameHandler('Mel', 49)}>Switch Name</button>
+ 		<button
+ 		  style={style}
+ 		  onClick={ () => this.switchNameHandler('Mel', 49)}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
