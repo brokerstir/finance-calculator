66982820de2cca2a1ae0b588929e102161ddea21
broker stir <take1risk@gmail.com>
2020-01-01 18:10:39 -0600

    Feature/render conditionally and dynamic content (#2)
    * render content conditionally with bool
    
    * use preferred way for conditional content

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   };
 
   switchNameHandler = (newName, newAge) => {
@@ -37,6 +38,11 @@ class App extends Component {
     });
   };
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
 
 
   render() {
@@ -49,28 +55,39 @@ class App extends Component {
   		cursor: 'pointer'
   	}
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, 'Chip', 77)}
+              changed={this.nameChangedHandler}
+            >
+              My Hobbies: Racing
+            </Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+            />
+        </div>
+        )
+    }
+
     return (
-      <div className="App">
+    <div className="App">
         <h1>Hi, I am a react app</h1>
- 		<button
- 		  style={style}
- 		  onClick={ () => this.switchNameHandler('Mel', 49)}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Chip', 77)}
-          changed={this.nameChangedHandler}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+   		<button
+   		  style={style}
+   		  onClick={this.togglePersonsHandler}>Toggle People
+      </button>
+      {persons}
      </div>
     );
   }
